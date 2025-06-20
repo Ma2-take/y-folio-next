f72a1e55da29b390d332b20e5b33513527ca2c4f
Ma2-take <240153nr@yse-c.net>
2025-06-20 14:29:47 +0900

    adminページ実装

---
+++ src/app/portfolio-list/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PortfolioPage = () => {
+  return (
+    <div>
+      <h1>ポートフォリオ一覧</h1>
+      <p>このページは現在準備中です。</p>
+    </div>
+  );
+};
+
+export default PortfolioPage; 
