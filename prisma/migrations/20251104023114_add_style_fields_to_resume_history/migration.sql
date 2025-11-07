f0507f79bda35240b8ce9f3d48e3e432eb8e9842
Ma2-take <240153nr@yse-c.net>
2025-11-07 09:18:35 +0900

    migration復旧

--- prisma/migrations/20251104023114_add_style_fields_to_resume_history/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE `resumereviewhistory` ADD COLUMN `audience` VARCHAR(191) NULL,
-    ADD COLUMN `honorific` VARCHAR(191) NULL,
-    ADD COLUMN `writingStyle` VARCHAR(191) NULL;
