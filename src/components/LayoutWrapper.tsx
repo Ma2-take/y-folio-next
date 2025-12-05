c5cb36238a069971f8a6b4d1505d6eb7c2ab3575
Ma2-take <240153nr@yse-c.net>
2025-12-05 09:23:57 +0900

    Layout変更

--- src/components/LayoutWrapper.tsx
+++ src/components/LayoutWrapper.tsx
@@ -8,10 +8,12 @@ const LayoutWrapper = ({ children }: { children: React.ReactNode }) => {
     const pathname = usePathname();
     const isNavHidden = pathname.startsWith('/admin') || pathname.startsWith('/dashboard') || pathname.startsWith('/recruiter') || pathname === '/portfolio' || pathname.startsWith('/portfolio/edit');
 
+    const wrapperClass = !isNavHidden ? 'pt-20 md:pt-24' : undefined;
+
     return (
         <>
             {!isNavHidden && <NavBar />}
-            {children}
+            <div className={wrapperClass}>{children}</div>
         </>
     );
 };
