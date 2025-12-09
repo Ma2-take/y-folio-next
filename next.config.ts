1b938be2818ed76dd126c0831c9aa919273743c1
yse-yoo <youhei_yoshikawa@yse-c.net>
2025-12-09 09:47:04 +0900

    next.config.ts の修正 ビルドエラー回避 CORS対策

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,32 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  typescript : {
+  typescript: {
     ignoreBuildErrors: true,
+  },
+  async headers() {
+    return [
+      {
+        // 全てのルートに適用
+        source: "/:path*",
+        headers: [
+          {
+            // ここが重要：ポップアップとの連携を許可する
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+          {
+            // APIアクセス許可（CORS対策）
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+        ],
+      },
+    ];
   },
 };
 
